424b494b0fb66098aa1493eeb839d7864e31f9f8
Leonel <leonelombardo1@gmail.com>
2023-01-30 18:44:23 -0300

    views: <Home/>

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../components/Navbar"
+import { Products } from "../components/Products"
+import { Wrapper } from "../components/Wrapper"
+
+export const Home = () => {
+    return (
+        <Wrapper>
+            <Navbar/>
+            <Products/>
+        </Wrapper>
+    )
+}
