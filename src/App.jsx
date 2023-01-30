b53833277addc23b7f99d3e01a64026f8666be39
Leonel <leonelombardo1@gmail.com>
2023-01-30 18:47:04 -0300

    no explanation for this commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import { Home } from "./views/Home"
+
+export const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
